081aab46e6656542966083898dc876316ea7fdbe
Malcom Fetus <sora.vista@gmail.com>
2020-05-11 22:55:13 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -1,2 +1,3 @@
+#here in this config/environment, this is where you set up loads and gem files from bundler MUST load first on an app
 require 'bundler/setup'
 Bundler.require(:default, :development) #the arguments here for the require method must be in order.
